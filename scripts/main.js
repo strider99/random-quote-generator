a454300faa668bb6ef3a6920a23579f8b5efdad7
strider99 <panigrahiomprakash@gmail.com>
2017-08-31 13:31:09 +0530

    add jquery and mainjs
    ajax call from api to get quotes and author

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+	var quote = '';
+	var author = '';
+	function getQuote () {
+		$.ajax({
+			headers : {
+				'X-Mashape-Key': 'irwLbOHG2fmshDcn5HEVj8j9hJ2vp1M8JOWjsn3RuZ4cjbnI9t',
+				Accept: 'application/json'
+
+			},
+			url: 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous&count=1',
+			success : function(response) {
+				
+				quote = response.quote;
+				author = response.author;
+				console.log(quote + ' and the author is ' + author);
+
+			},
+			error : function(err){
+				console.log("There was an error + ", err);
+			}
+		});		
+	}
+
+	getQuote();
+
+});
